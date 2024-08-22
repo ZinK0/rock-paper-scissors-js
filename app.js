1180800ce2a6a5190601dbf173989e5b68450d0d
ZIN KO <zinko@ZINs-Mac-Pro.local>
2024-08-22 22:22:14 +0630

    Added humanChoice function to get user move

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
-// Random choice for computer
+// Initial Score for both human and computer
+let humanScore = 0;
+let computerScore = 0;
 
+// Random choice for computer
 function getComputerChoice() {
   let gameMode = ["rock", "paper", "scissor"];
-
   let computerChoice = Math.floor(Math.random() * gameMode.length + 1) - 1;
-
   return gameMode[computerChoice];
 }
-getComputerChoice();
+
+// Take human input move
+function getHumanChoice() {
+  let humanChoice = prompt("Choose your move => 'rock', 'paper', 'scissor' : ");
+  return humanChoice.toLowerCase;
+}
+
+// function playRound(humanChoice, computerChoice) {
+//     if (humanChoice == 'rock' || ) {
+
+//     }
+// }
