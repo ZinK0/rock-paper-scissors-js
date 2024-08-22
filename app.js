a5c5ecc0405be40ca24e8e466b553c41169ca56c
Zin KO <zinko.it@outlook.com>
2024-08-22 23:21:54 +0630

    Added final function and some styles

--- app.js
+++ app.js
@@ -11,12 +11,57 @@ function getComputerChoice() {
 
 // Take human input move
 function getHumanChoice() {
-  let humanChoice = prompt("Choose your move => 'rock', 'paper', 'scissor' : ");
-  return humanChoice.toLowerCase;
+  let humanChoice = prompt(
+    "Choose your move => 'rock', 'paper', 'scissor' : "
+  ).toLowerCase;
+  return humanChoice;
 }
 
-// function playRound(humanChoice, computerChoice) {
-//     if (humanChoice == 'rock' || ) {
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice == "rock" || computerChoice == "rock") {
+    console.log("It's a tie. Nobody Win.");
+  } else if (humanChoice == "rock" || computerChoice == "paper") {
+    console.log("You lose! Paper beats Rock");
+    computerScore++;
+  } else if (humanChoice == "rock" || computerChoice == "scissor") {
+    console.log("You Win! Rock beats Scissor");
+    humanScore++;
+  } else if (humanChoice == "paper" || computerChoice == "paper") {
+    console.log("It's a tie. Nobody Win.");
+  } else if (humanChoice == "paper" || computerChoice == "scissor") {
+    console.log("You Lose! Scissor beats Paper");
+    computerScore++;
+  } else if (humanChoice == "paper" || computerChoice == "rock") {
+    console.log("You Win! Paper beats Rock");
+    humanScore++;
+  } else if (humanChoice == "scissor" || computerChoice == "rock") {
+    console.log("You Lose! Rock beats Scissor");
+    computerScore++;
+  } else if (humanChoice == "scissor" || computerChoice == "paper") {
+    console.log("You Win! Scissor beats Paper");
+    humanScore++;
+  } else if (humanChoice == "scissor" || computerChoice == "scissor") {
+    console.log("It's a tie. Nobody Win.");
+  }
+}
 
-//     }
-// }
+// playRound(humanChoice, computerChoice);
+
+let gamePlay = 5;
+
+function playGame() {
+  console.log("This game will be last 5 rounds... ");
+  while (gamePlay > 0) {
+    let humanChoice = getHumanChoice();
+    let computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
+    gamePlay--;
+  }
+  if (humanScore > computerScore) {
+    console.log("You Win in 5 rounds ... ");
+  } else {
+    console.log("Computer Win in 5 rounds...");
+  }
+}
+
+// playGame();
